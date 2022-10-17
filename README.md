78a3be636f287d266e8cdb60b178d379dd0dbab0
Issei Takabe <i_takabe@cisjp.com>
2022-10-17 18:57:16 +0900

    諸々修正

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # DB2接続確認用サンプルサイト
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## はじめに
 
+Expressを利用したモック用のWebサーバとなります。
 Node.jsにて動作しますので、テスト実行する場合は事前に [ダウンロードサイト](https://nodejs.org/ja/download/) よりNode.jsのインストーラを取得・インストールを行ってください。
 
 インストール後、`cd test-db2-web` にて本ディレクトリに移動し、以下コマンドを実行します。
 ```bash
 
 npm i # 前提モジュールのインストール
-npm run dev # Webサイト起動(デバック実行)
+node app.js # Webサイト起動
 ```
 Webサイトが正常に起動すると、`Server running on port: 3000` と表示されますので、ブラウザにて [http://localhost:3000](http://localhost:3000) にアクセスしてください。
 
@@ -56,7 +56,7 @@ sudo docker rm db2
 
 ### 公式
 
-* [Create Next App | Next.js](https://nextjs.org/docs/api-reference/create-next-app)
+* [Express "Hello World" example](https://expressjs.com/en/starter/hello-world.html)
 * [ibm_db - npm](https://www.npmjs.com/package/ibm_db)
 * [node-ibm_db ドライバーのインストールの検証 - IBM Documentation](https://www.ibm.com/docs/ja/db2/11.5?topic=nodejs-verifying-node-db-driver-installation)
 * [ibmcom/db2 - Docker Image | Docker Hub](https://hub.docker.com/r/ibmcom/db2)
@@ -67,12 +67,3 @@ sudo docker rm db2
 * [node.jsでdb2に接続 | 株式会社CONFRAGE ITソリューション事業部](https://confrage.jp/node-js%E3%81%A7db2%E3%81%AB%E6%8E%A5%E7%B6%9A/)
 * [Expressにおけるejsの使い方 - Qiita](https://qiita.com/kamihork/items/1b13d2157979d1837849)
 * [【備忘録】Docker版 Db2環境 取得手順 - Qiita](https://qiita.com/Haruka-Ogawa/items/0a4696ded4fa40e5e983)
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,28 @@ const ibmdb = require('ibm_db');
 //DB接続文字列
 const db_conn_str="DATABASE=testdb;HOSTNAME=localhost;UID=db2inst1;PWD=password;PORT=50000;PROTOCOL=TCPIP"
 
+class Query {
+  // str: description
+  constructor(str, description) {
+    this.str = str;
+    this.description = description;
+  }
+}
+
+const queryArr= [];
+queryArr["version"] = new Query("SELECT T.SERVICE_LEVEL FROM TABLE(ENV_GET_INST_INFO()) AS T;","DB2バージョン表示"),
+queryArr["table"] = new Query("SELECT * FROM SYSIBM.TABLES;","テーブル表示"),
+queryArr["database"] = new Query("LIST DATABASE DIRECTORY;","データベースリスト表示"),
+
+
 router.get("/", (req, res) => {
   const data = {
-    title: "サンプルタイトル",
+    title: "トップページ",
+    text: "DB接続確認用のサンプルWebページです。以下リンクにてDB2との接続確認ができます。",
     items: [
-      { name: "ABC" },
-      { name: "DEF" },
-      { name: "GHI" }
+      { name: `<a class="nav-link" href="/version">${queryArr["version"].description}</a>` },
+      { name: `<a class="nav-link" href="/table">${queryArr["table"].description}</a>` },
+      { name: `<a class="nav-link" href="/database">${queryArr["database"].description}</a>` },
     ]
   };
   // data を ejs に渡す
@@ -17,29 +32,44 @@ router.get("/", (req, res) => {
 });
 
 router.get("/version", (req, res) => {
+  con_db(req,res,queryArr.version);
+});
+
+router.get("/table", (req, res) => {
+  con_db(req,res,queryArr.table);
+});
+
+router.get("/database", (req, res) => {
+  con_db(req,res,queryArr.database);
+});
+
+
+function con_db(req, res, query){
   ibmdb.open(db_conn_str, function (err, conn) {
     console.log('db connection started.');
     if (err) {
       console.log(err);
-      res.render("index.ejs", {title: "DB接続エラー", items: [{name: err}]});
+      res.status(500);
+      res.render("index.ejs", {title: query.description, text: `DB接続に失敗しました。以下エラーをご確認ください。`, items: [{name: err}]});
+      //res.json(err)
     }
-    conn.query('SELECT T.SERVICE_LEVEL FROM TABLE(ENV_GET_INST_INFO()) AS T;', function (err, data) {
+    conn.query(query.str, function (err, data) {
       if (err) {
         console.log(err);
-        res.render("index.ejs", {title: "クエリエラー", items: [{name: err}]});
+        res.status(500);
+        res.render("index.ejs", {title: query.description, text: `クエリの発行でエラーが発生しました。<br />発行クエリ：${query.str}`, items: [{name: err}]});
         //res.json(err)
       }
 
       //res.json(data);
       console.log(data);
-      res.render("index.ejs", {title: "出力結果", items: [{name: JSON. stringify(data)}]});
+      res.render("index.ejs", {title: query.description, text: `クエリの発行に成功しました。<br />発行クエリ：${query.str}`, items: [{name: JSON. stringify(data)}]});
 
       conn.close(function () {
         console.log('db connection closed.');
       });
     });
   });
-});
-
+}
 
 module.exports = router;
